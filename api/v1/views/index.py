456919d8f4bcef0c38ffed3ce596741732489cb8
Noahxyzs <noahsolomon528@yahoo.com>
2023-07-30 09:17:14 -0700

    view

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
-"""Definition of index"""
+""" API test stats"""
 from api.v1.views import app_views
-from flask import Flask
+from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status():
-    """Returns status of App"""
+    """status"""
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """stats"""
+    return jsonify({
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+        })
